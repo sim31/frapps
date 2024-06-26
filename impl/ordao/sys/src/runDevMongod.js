e99f8100f6e5cc94f6ea53a778ff3124fcd614c9
Tadas <vtadas25@gmail.com>
2024-06-26 13:45:09 +0300

    Fix mongod launch command for bare repo

--- impl/ordao/sys/src/runDevMongod.js
+++ impl/ordao/sys/src/runDevMongod.js
@@ -2,4 +2,5 @@ import shelljs from "shelljs";
 
 const dbpath = './tmp/mongodb/dev'
 shelljs.exec(`mkdir -p ${dbpath}`);
+// shelljs.exec(`rm -rf ${dbpath}/*`)
 shelljs.exec(`mongod --dbpath=${dbpath}`);
