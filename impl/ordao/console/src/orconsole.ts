a0dafbaa003ae965a4eba93d1d38dc49b5ed6d52
Tadas <vtadas25@gmail.com>
2024-07-30 17:57:43 +0300

    Navigation through docs using console

--- impl/ordao/console/src/orconsole.ts
+++ impl/ordao/console/src/orconsole.ts
@@ -1,9 +1,32 @@
 import { ORClient, defaultConfig, Config } from "orclient/index.js";
 import { ORContext, ConfigWithOrnode } from "ortypes/orContext.js";
+import { stringify } from "ts-utils";
 
 export class ORConsole extends ORClient {
+  private _methods: string[];
+  private _docPath = "/classes/ORConsole.html";
+
   constructor(context: ORContext<ConfigWithOrnode>, cfg: Config = defaultConfig) {
     super(context, cfg);
+
+    this._methods = Object.getOwnPropertyNames(ORClient.prototype)
+      .filter(m => m[0] != '_');
+    console.log("methods: ", stringify(this._methods));
+  }
+
+  help(method?: string) {
+    const methodMatch = method === undefined
+      ? undefined
+      : this._methods.find(m => m === method);
+
+    if (methodMatch === undefined) {
+      console.log(this._methods);
+    } else {
+      window.location.hash = methodMatch;
+      if (window.location.pathname !== this._docPath) {
+        window.location.pathname = this._docPath;
+      }
+    }
   }
 
 }
