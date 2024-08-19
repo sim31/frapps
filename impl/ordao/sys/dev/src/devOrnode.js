a7b98c11801785922652f79a71b1786362976416
Tadas <vtadas25@gmail.com>
2024-08-19 10:27:01 +0300

    Move ordao-sys under ordao/sys/dev

--- impl/ordao/sys/dev/src/devOrnode.js
+++ impl/ordao/sys/dev/src/devOrnode.js
@@ -17,7 +17,7 @@ async function main() {
   // shelljs.exec("cd $npm_package_config_ornode && ORNODE_CFG_PATH=../ordao/sys/tmp/ornode-dev-cfg.json npx nodemon --watch node_modules/ortypes node_modules/ts-utils dist/index.js");
   console.log("we're in devOrnode!");
   shelljs.exec(`cd $npm_package_config_ornode && \
-    ORNODE_CFG_PATH=../ordao/sys/tmp/ornode-dev-cfg.json \
+    ORNODE_CFG_PATH=$npm_package_config_ornode_to_cfg_path \
     \
     npx nodemon \
     --watch ../ortypes/dist \
