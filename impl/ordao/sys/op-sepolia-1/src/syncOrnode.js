6146b7092bd7a805f9add84ac9f7498faec5afcf
Tadas <vtadas25@gmail.com>
2024-10-04 07:08:52 +0300

    Sync script for op-sepolia-1

---
+++ impl/ordao/sys/op-sepolia-1/src/syncOrnode.js
@@ -0,0 +1,29 @@
+import jsonfile from "jsonfile";
+import shelljs from "shelljs";
+
+const config = jsonfile.readFileSync("../ornode-cfg.json");
+
+console.log("Args: ", process.argv);
+
+config.ornode.sync = {
+  fromBlock: process.argv[2],
+  toBlock: process.argv[3],
+  stepRange: process.argv[4]
+}
+
+jsonfile.writeFileSync("./tmp/ornode-sync-cfg.json", config);
+
+shelljs.exec(`cd $npm_package_config_ornode && \
+  ORNODE_CFG_PATH=../ordao/sys/op-sepolia-1/tmp/ornode-sync-cfg.json \
+  \
+  npx nodemon \
+  --watch ../../../ortypes/dist \
+  --watch ../../../ts-utils/dist \
+  --watch ../../../respect1155/sc/dist \
+  --watch ../../../orec/dist/ \
+  --watch ./ \
+  \
+  --delay 5
+  \
+  dist/index.js`
+);
