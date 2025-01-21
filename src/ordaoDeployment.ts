70885d85d9efa89905bb08931bbfab2004cf9402
Tadas <vtadas25@gmail.com>
2025-01-21 14:11:15 +0200

    Add OrdaoExisting deployment type

--- src/ordaoDeployment.ts
+++ src/ordaoDeployment.ts
@@ -3,7 +3,7 @@ import { zBaseDeployment } from "./baseDeployment";
 import { zBytes, zEthAddress } from "@ordao/ortypes";
 import { zRespectHolders } from "./respectHolders";
 
-export const zOrdaoBaseDeployment = zBaseDeployment.extend({
+export const zNewOrdaoBaseDeployment = zBaseDeployment.extend({
   voteLength: z.number(),
   vetoLength: z.number(),
   voteThreshold: z.number(),
@@ -16,6 +16,7 @@ export const zOrdaoBaseDeployment = zBaseDeployment.extend({
  * * Parent respect deployed and issued, owner set to 0 (e.g.: buildium) (OrdaoParentCreated)
  * * Parent respect already exists (e.g.: ZAO, OF, OH, op-sepolia) (OrdaoParentExists)
  * * Parent respect deployed and issued, owner set to orec (so that orec is an oracle for respect of parent) (e.g.: EF) (OrdaoParentOraclized)
+ * * ORDAO already has onchain deployment (OrdaoExisting)
  */
 
 
@@ -37,24 +38,37 @@ export const zParentOraclized = z.object({
   mintData: zBytes.optional(),
 });
 
-export const zOrdaoParentCreated = zOrdaoBaseDeployment.extend({
+export const zOrdaoParentCreated = zNewOrdaoBaseDeployment.extend({
   module: z.literal("OrdaoParentCreated"),
   parent: zParentCreated
 });
 
-export const zOrdaoParentExists = zOrdaoBaseDeployment.extend({
+export const zOrdaoParentExists = zNewOrdaoBaseDeployment.extend({
   module: z.literal("OrdaoParentExists"),
   parent: zParentExists
 });
 
-export const zOrdaoParentOraclized = zOrdaoBaseDeployment.extend({
+export const zOrdaoParentOraclized = zNewOrdaoBaseDeployment.extend({
   module: z.literal("OrdaoParentOraclized"),
   parent: zParentOraclized
+});
+
+export const zOrdaoContracts = z.object({
+  oldRespect: zEthAddress,
+  newRespect: zEthAddress,
+  orec: zEthAddress
+});
+export type OrdaoContracts = z.infer<typeof zOrdaoContracts>;
+
+export const zOrdaoExisting = zBaseDeployment.extend({
+  module: z.literal("OrdaoExisting"),
+  contracts: zOrdaoContracts
 });
 
 export const zOrdaoDeployment = z.union([
   zOrdaoParentCreated,
   zOrdaoParentExists,
-  zOrdaoParentOraclized
+  zOrdaoParentOraclized,
+  zOrdaoExisting
 ]);
 
