7dda946db45eabf15341adcef69b102ad9f70e8a
sim31 <vtadas25@gmail.com>
2025-02-05 10:33:35 +0200

    Deployment for of2

--- src/types/baseFrapp.ts
+++ src/types/baseFrapp.ts
@@ -3,7 +3,7 @@ import { zBaseApp } from "./baseApp.js";
 import { zBaseDeploymentCfg } from "./baseDeploymentCfg.js";
 
 export const zBaseFrapp = z.object({
-  id: z.string().regex(/^[a-z-]{2,12}$/).describe("Short identifier for the fractal. Must be unique."),
+  id: z.string().regex(/^[a-z0-9-]{2,12}$/).describe("Short identifier for the fractal. Must be unique."),
   fullName: z.string().describe("Full name / title of this fractal."),
   description: z.string().optional(),
   deploymentCfg: zBaseDeploymentCfg.optional(),
